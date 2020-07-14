7c731efadfad78b1282f1f18bb48222d68c01f97
onadj <oliver.nad@gmail.com>
2020-07-14 06:39:38 +0000

    secret key

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
+if os.path.exists("env.py"):
+    import env
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -28,6 +30,6 @@ def insert_review():
 
 if __name__ == '__main__':
     app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
+        host(os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
