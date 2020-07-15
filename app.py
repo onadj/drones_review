d4e59289b453a177f23e925f75d252607e9defb5
onadj <oliver.nad@gmail.com>
2020-07-15 11:39:06 +0000

    edit and delete review buttons

--- app.py
+++ app.py
@@ -33,12 +33,69 @@ def insert_review():
     reviews.insert_one(request.form.to_dict())
     return redirect(url_for('get_reviews'))
 
+
 @app.route('/edit_review/<review_id>')
 def edit_review(review_id):
     the_review =  mongo.db.reviews.find_one({"_id": ObjectId(review_id)})
     all_categories =  mongo.db.categories.find()
     return render_template('edit_review.html', review=the_review,
                            categories=all_categories)
+
+
+@app.route('/update_review/<review_id>', methods=["POST"])
+def update_review(review_id):
+    reviews = mongo.db.reviews
+    reviews.update( {'_id': ObjectId(review_id)},
+    {
+        'category_name': request.form.get('category_name'),
+        'name': request.form.get('name'),
+        'review': request.form.get('review')  
+    })
+    return redirect(url_for('get_reviews'))
+
+
+@app.route('/delete_review/<review_id>')
+def delete_review(review_id):
+    mongo.db.reviews.remove({'_id': ObjectId(review_id)})
+    return redirect(url_for('get_reviews'))
+
+
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html',
+                           categories=mongo.db.categories.find())
+
+
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+    category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
+
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    category_doc = {'category_name': request.form.get('category_name')}
+    mongo.db.categories.insert_one(category_doc)
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/add_category')
+def add_category():
+    return render_template('addcategory.html')
 
 
 if __name__ == '__main__':
