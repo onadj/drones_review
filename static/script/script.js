ac3ec1106c9e6f5120dc64154e0680a9f864ea6f
onadj <oliver.nad@gmail.com>
2020-07-18 14:34:43 +0000

    stars

---
+++ static/script/script.js
@@ -0,0 +1,67 @@
+// Initial Ratings
+    const ratings = {
+      drone1: 4.7,
+      drone2: 3.4,
+      drone3: 2.3,
+      drone4: 3.6,
+      drone5: 4.1,
+      drone6: 3.4,
+      drone7: 3.4,
+      drone8: 3.4,
+      drone9: 3.4,
+    }
+    
+let starIcon = "<i class='fas fa-star'></i>";
+    // Total Stars
+    const starsTotal = 5;
+
+    // Run getRatings when DOM loads
+    document.addEventListener('DOMContentLoaded', getRatings);
+
+    // Form Elements
+    const productSelect = document.getElementById('product-select');
+    const ratingControl = document.getElementById('rating-control');
+
+    // Init drone
+    let product;
+
+    // Drone select change
+    productSelect.addEventListener('change', (e) => {
+      product = e.target.value;
+      // Enable rating control
+      ratingControl.disabled = false;
+      ratingControl.value = ratings[product];
+    });
+
+    // Rating control change
+    ratingControl.addEventListener('blur', (e) => {
+      const rating = e.target.value;
+
+      // Make sure 5 or under
+      if (rating > 5) {
+        alert('Please rate 1 - 5');
+        return;
+      }
+
+      // Change rating
+      ratings[product] = rating;
+
+      getRatings();
+    });
+
+    // Get ratings
+    function getRatings() {
+      for (let rating in ratings) {
+        // Get percentage
+        const starPercentage = (ratings[rating] / starsTotal) * 100;
+
+        // Round to nearest 10
+        const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
+
+        // Set width of stars-inner to percentage
+        document.querySelector(`.${rating} .stars-inner`).style.width = starPercentageRounded;
+
+        // Add number rating
+        document.querySelector(`.${rating} .number-rating`).innerHTML = ratings[rating];
+      }
+    }
