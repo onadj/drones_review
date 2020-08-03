244d6eb1f67f4dad5302e286e951a8e92836cf0e
onadj <oliver.nad@gmail.com>
2020-08-03 19:50:34 +0000

    fixed read button

--- static/script/readReviews.js
+++ static/script/readReviews.js
@@ -1,6 +1,6 @@
 $('.readReview').click(function(e) {
   e.preventDefault();
   $(this).text(function(i, t) {
-    return t == 'Close' ? 'Read Review' : 'Close';
+    return t == 'Close' ? 'Read' : 'Close';
   }).prev('.more-cont').slideToggle()
 });;
